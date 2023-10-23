59ad31543cea6579a1981d3f11f989075a0341a4
Matt Andrews <mattandrews@Matts-MacBook-Pro-2.local>
2023-10-23 12:28:45 +1100

    Remove initialize function

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,9 @@ use wasm_bindgen::prelude::*;
 const SENDER: OnceCell<i32> = OnceCell::new();
 
 #[wasm_bindgen]
-pub fn initialize() {
-    SENDER.set(1).unwrap();
-}
-
-#[wasm_bindgen]
 pub fn get() -> i32 {
+    SENDER.set(1).unwrap();
+
     let binding = SENDER;
 
     let Some(n) = binding.get() else {
